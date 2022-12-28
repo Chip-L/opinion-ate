6d2b137224af63115059cf96748a87c5678b2af8
clong desktop <chip_long@yahoo.com>
2022-12-27 22:33:37 -0700

    added in test badges and tied Cypress to my cloud dashboard

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  projectId: '1hj2om',
   e2e: {
     baseUrl: 'http://localhost:3000',
-    // setupNodeEvents(on, config) {
-    //   // implement node event listeners here
-    // },
   },
 });
