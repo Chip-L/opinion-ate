dd1f966fb029533ca4e0d5dd1ed02bbb9ced703f
clong desktop <chip_long@yahoo.com>
2022-12-28 19:42:09 -0700

    Load restaurants upon first rendering RestaurantList

--- src/components/RestaurantList/RestaurantList.tsx
+++ src/components/RestaurantList/RestaurantList.tsx
@@ -1,3 +1,13 @@
-export function RestaurantList() {
+import { useEffect } from 'react';
+
+interface RestaurantListProps {
+  loadRestaurants?: () => void;
+}
+
+export function RestaurantList({ loadRestaurants }: RestaurantListProps) {
+  useEffect(() => {
+    loadRestaurants();
+  }, [loadRestaurants]);
+
   return <div>RestaurantList</div>;
 }
